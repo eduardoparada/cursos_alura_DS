9a299b07fde6b38161856721d33a5b01a0e75133
Eduardo Parada <102472995+eduardoparada@users.noreply.github.com>
2024-01-27 12:12:58 -0300

    Add files via upload

--- MLOps_ML_APIs/MLops - ML e API/Primeira_API/main.py
+++ MLOps_ML_APIs/MLops - ML e API/Primeira_API/main.py
@@ -1,5 +1,19 @@
-from flask import Flask
+from flask import Flask, request, jsonify #request faz um request JSON e o jsonify retorna um valor de arquivo JSON
 from textblob import TextBlob
+
+#Criando toda a estrutura de código para nova api COTACAO   
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+
+df = pd.read_csv('casas.csv',sep=',')
+coluna =["tamanho", "ano","garagem"]
+x= df.drop('preco', axis =1)
+y = df['preco']
+x_treino, x_teste, y_treino, y_teste = train_test_split(x,y,test_size=0.3, random_state = 42)
+modelo = LinearRegression()
+modelo.fit(x_treino,y_treino)
+
 
 app = Flask (__name__) #criando a variável app, chamando o flask pra criar a API, __name__ é uma variável especial
 
@@ -14,6 +28,15 @@ def sentimento (frase):
     tb_en=tb.translate(from_lang='pt',to='en')
     polaridade = tb_en.sentiment.polarity
     return 'polaridade: {}'.format(polaridade)
+
+@app.route('/cotacao/', methods =['POST'])
+
+def cotacao():
+    dados = request.get_json()
+    dados_input = [dados[col]for col in coluna]
+    preco = modelo.predict([dados_input])
+    return jsonify(preco=preco[0])#jsonify retorna valor em JSON
+
 
 app.run(debug=True)#executável, o 'debug=True' serve pra que toda vez que esse código seja modificado, a própria API entenda
                    #e reinicie a execução
